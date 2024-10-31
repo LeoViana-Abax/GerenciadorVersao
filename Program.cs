9cc76067c5a5e7dc5865d1c4da901d6978acd774
Filipe Alan <35692798+FilipeAlan@users.noreply.github.com>
2024-10-31 18:06:59 -0300

    Primeira versão do controlador de versões

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Application.Manipuladores;
+using GerenciadorVersao.Components;
+using MudBlazor.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
+builder.Services.AddScoped<ManipuladorVersao>();
+builder.Services.AddMudServices();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
